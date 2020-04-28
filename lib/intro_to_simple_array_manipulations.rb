aefe9cf4eb1d39dc90a164a995e083ca5eb264ef
Greem <orangenewyorkcreativeagency@gmail.com>
2020-04-28 14:39:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,20 +1,24 @@
+def using_concat (arr1, arr2)
+  arr1.concat(arr2)
+end
 =begin
-describe "using_concat" do
+describe "using_insert" do
   before(:each) do
-    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["sports cars", "flatiron school"]
-    @all_my_favs = using_concat(@my_favorite_things, more_favs)
+    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
+    @new_array = using_insert(@list_of_programming_languages, @another_language)
   end
-   it "takes in two arguments of two different arrays and uses the concat method to add the contents of the second array to the first" do
-    expect(@all_my_favs).to eq(["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"])
-   end
 
-   it "increases the length of the first array" do
-    expect(@my_favorite_things.length).to be > 2
-   end
+  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do
+    expect(@new_array[4]).to eq(@another_language)
+  end
+
+  it "increases the length of the array" do
+    expect(@new_array.length).to eq(8)
+  end
 end
 =end
 
-def using_concat (arr1, arr2)
-  arr1.concat(arr2)
+def using_insert (arr, ele)
+  arr.insert(ele)
 end
